6e4343989bfe9a017c433037c449cf1b1fcf46cf
tejasva-087 <khandelwaltejasva@gmail.com>
2025-05-11 16:50:28 +0530

    Added some more features, getting all transactions, deleting transaciton and updating the transaction

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const Transaction = require('../models/transactionModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -11,6 +13,63 @@ exports.addTransaction = catchAsync(async (req, res) => {
     status: 'success',
     data: {
       transaction,
+    },
+  });
+});
+
+exports.deleteTransaction = catchAsync(async (req, res, next) => {
+  await Transaction.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.getAllTransaction = catchAsync(async (req, res) => {
+  const transactions = await Transaction.find({ _id: req.user.id });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      transactions,
+    },
+  });
+});
+
+exports.updateTransaction = catchAsync(async (req, res, next) => {
+  const transaction = await Transaction.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      transaction,
+    },
+  });
+});
+
+exports.totalSpent = catchAsync(async (req, res) => {
+  const totalSpend = await Transaction.aggregate({
+    $match: {
+      user: req.user.id,
+    },
+    $group: {
+      _id: null,
+      totalSpent: { $sum: '$amount' },
+    },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      totalSpend,
     },
   });
 });
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const { protectedRoute } = require('../controllers/authController');
-const { addTransaction } = require('../controllers/transactionController');
+const {
+  addTransaction,
+  deleteTransaction,
+  getAllTransaction,
+  updateTransaction,
+  totalSpent,
+} = require('../controllers/transactionController');
 
 const router = express.Router();
 
-router.route('/').post(protectedRoute, addTransaction);
+router
+  .route('/')
+  .post(protectedRoute, addTransaction)
+  .get(protectedRoute, getAllTransaction);
+
+router
+  .route('/:id')
+  .delete(protectedRoute, deleteTransaction)
+  .patch(protectedRoute, updateTransaction);
+
+router.get('/totalSpent', protectedRoute, totalSpent);
 
 module.exports = router;
